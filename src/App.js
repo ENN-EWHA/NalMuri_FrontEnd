4b23d0eb6dd489ee552dd3b6f90ec78bd3f578a7
asaei623 <86418308+asaei623@users.noreply.github.com>
2022-11-30 11:58:12 +0900

    global style 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignupPage from "./pages/SignupPage";
+import { createGlobalStyle } from "styled-components";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <Router>
+            <GlobalStyle />
+            <Routes>
+                <Route path="/signup" element={<SignupPage />} />
+            </Routes>
+        </Router>
+    );
 }
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+
+    font-family: 'NanumSquareOTF_acR';
+    src:url("./assets/font/NanumSquareOTF_acR.otf");
+    font-weight: 400;
+    font-size: 18px;
+    }
+`;
 
 export default App;
